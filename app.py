9e68cb5bd3c039c54a7f1fa84ba270afab6df859
nellito2811 <124921954+nellito2811@users.noreply.github.com>
2023-05-29 23:14:57 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import telebot
 from config import keys, TOKEN
-from extensions import ConvertionExeption, CryptoConverter 
+from extensions import APIExeption, CryptoConverter 
 
 
 
@@ -26,23 +26,23 @@ def values(message: telebot.types.Message):
 @bot.message_handler(content_types=['text', ])
 def convert(message: telebot.types.Message):
     try:    
-        values = message.text.split(' ')
+        values = message.text.split()
 
         if len(values) > 3:
-            raise ConvertionExeption('Слишком много параметров')
+            raise APIExeption('Слишком много параметров')
 
         if len(values) < 3:
-            raise ConvertionExeption('Слишком мало параметров')    
+            raise APIExeption('Слишком мало параметров')    
 
         quote, base, amount = values
         new_price = CryptoConverter.convert(quote, base, amount)
-    except ConvertionExeption as e:
-        bot.reply_to(message, f'Ошибка пользователя.\n{e}')
+    except APIExeption as e:
+        bot.reply_to(message, f'Ошибка в запросе пользователя.\n{e}')
     except Exception as e:
         bot.reply_to(message, f'Не удалось обработать команду\n{e}')
     else:    
         text = f'Цена {amount} {quote} в {base} - {new_price}'
-        bot.send_message(message.chat_id, text)
+        bot.send_message(message.chat.id, text)
 
 
 
--- extensions.py
+++ extensions.py
@@ -3,7 +3,7 @@ import json
 from config import keys
 
 
-class ConvertionExeption(Exception):
+class APIExeption(Exception):
     pass
 
 
@@ -11,39 +11,35 @@ class CryptoConverter:
     @staticmethod
     def convert(quote: str, base: str, amount: str):
         if quote == base:
-            raise ConvertionExeption(f'Невозможно перевести одинаковые валюты {base}.')
+            raise APIExeption(f'Невозможно перевести одинаковые валюты {base}.')
 
         try:
             quote_ticker = keys[quote]
         except KeyError:
-            raise ConvertionExeption(f'Не удалось обработать валюту {quote}.')
+            raise APIExeption(f'Не удалось обработать валюту {quote}.')
 
         try:
             base_ticker = keys[base]
         except KeyError:
-            raise ConvertionExeption(f'Не удалось обработать валюту {base}.')        
+            raise APIExeption(f'Не удалось обработать валюту {base}.')        
 
         try:
-            amount = float(amount)
+            amount = float(amount.replace(",", "."))
         except ValueError:
-            raise ConvertionExeption(f'Не удалось обработать количество {amount}')
+            raise APIExeption(f'Не удалось обработать количество {amount}')
     
-        r = requests.get(f'https://api.exchangeratesapi.io/latest?base={base_ticker}&symbols={quote_ticker}')
-        resp = json.loads(r.content)
-        print(resp)
-        new_price = resp['rates'][quote_ticker] * amount
+               
         
+        url = (f"https://api.apilayer.com/exchangerates_data/convert?to={base_ticker}&from={quote_ticker}&amount={amount}")
         
-        # url = f"https://api.apilayer.com/exchangerates_data/convert?to={quote_ticker}&from={base_ticker}&amount={amount}"
-        
-        # payload = {}
-        # headers= {
-        # "apikey": "0vhXB7bb29gh6Ab0aFn1HCm5TbmZuwXz"
-        # }
+        payload = {}
+        headers= {
+        "apikey": "0vhXB7bb29gh6Ab0aFn1HCm5TbmZuwXz"
+        }
 
-        # r = requests.request("GET", url, headers=headers, data = payload)
-        # resp = json.loads(r.content)
-        # new_price = resp[result][total] * amount
+        r = requests.request("GET", url, headers=headers, data = payload)
+        resp = json.loads(r.content)
+        new_price = resp["result"]
         
         
         
